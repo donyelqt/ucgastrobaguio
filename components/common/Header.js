05f381cd3f7bdf7d0ea439ab6662578ce2cfb500
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-02-05 10:06:51 +0800

    bago ulit

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,9 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
     <>
-       <h1>Header</h1>
+       <header>
+        <div className='container'>
+          <div className=''>
+            <Link href='/'></Link>
+          </div>
+          <nav>
+            <Link href='/'>Home</Link>
+            <button>book a consultation</button>
+          </nav>
+        </div>
+       </header>
     </>
   )
 }
