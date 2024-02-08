a73d72c6693d5fe1ddbfeaf2bac1b2fd12312d9c
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-02-08 22:13:25 +0800

    neww ulit ahhaaa

--- components/common/Title.js
+++ components/common/Title.js
@@ -1,28 +1,39 @@
-export const TitleLogo = ({ title, caption, className }) => {
-    return (
-      <h1 className={`${className} title-logo`}>
-        <span>{caption}</span>
-        <img src="/UC_Official_Logo.png" alt={title} width="350" height="110"  />
-      </h1>
-    );
-  };
+import Image from 'next/image';
 
-  export const Logo = ({ title, caption, className }) => {
-    return (
-      <h1 className={`${className} logo`}>
-        <span>{caption}</span>
-        <img src="/uclogonew.png" alt={title} width="120" height="150"  />
-      </h1>
-    );
-  };
+// ...
+
+export const TitleLogo = ({ title, caption, className }) => {
+  return (
+    <h1 className={`${className} title-logo`}>
+      <span>{caption}</span>
+      <Image
+        src="/UC_Official_Logo.png"
+        alt={title}
+        width={350}
+        height={110}
+      />
+    </h1>
+  );
+};
+
+export const Logo = ({ title, caption, className }) => {
+  return (
+    <h1 className={`${className} logo`}>
+      <span>{caption}</span>
+      <Image
+        src="/uclogonew.png"
+        alt={title}
+        width={120}
+        height={150}
+      />
+    </h1>
+  );
+};
 
 export const TitleSm = ({ title }) => {
-    return <h1 className={`title-sm`}>{title}</h1>
-  }
+  return <h1 className={`title-sm`}>{title}</h1>;
+};
 
-
-  export const Title = ({ title, className }) => {
-    return <h1 className={`${className} title`}>{title}</h1>
-  }
-
-
+export const Title = ({ title, className }) => {
+  return <h1 className={`${className} title`}>{title}</h1>;
+};
