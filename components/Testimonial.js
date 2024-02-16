11a4999d0245de9c95d3e6c905ab55c55aaf5ed3
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-02-16 15:20:02 +0800

    hello world sheshhhhhhhhhhhh

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -8,7 +8,7 @@ const Testimonial = () => {
         <div className='container'>
           <div className='heading-title'>
             <Title title="PATIENTS REVIEW FOR THE HOSPITALS IN BAGUIO CITY." />
-            <p><span className='white'>This platform aims to provide convenient access to medical services and information for the residents of Baguio City. With this online platform, patients can schedule appointments, access their medical records, and even consult with doctors remotely. It also allows hospitals to streamline their operations and improve efficiency in delivering healthcare services.</span></p>
+            <p><span className='white'></span></p>
           </div> 
         </div>
        </section>
